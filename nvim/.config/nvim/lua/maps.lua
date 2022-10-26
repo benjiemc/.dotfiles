ae9e5736324a633315c22d742366bc516757e01b
benjiemc <benjiemc@telus.net>
2022-10-26 12:07:43 +0100

    Remove config from unused packages
    Coc keybinds and ripgrep/FZF mappings are removed as these packages have
    been replaced by neovim specific tools.

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -5,10 +5,6 @@ map('n', '<Space>', '', {})
 vim.g.mapleader = ' '
 
 options = { noremap = true }
-map('n', '<silent> gd', '<Plug>(coc-definition)', {})
-map('n','<silent> gy', '<Plug>(coc-type-definition)', {})
-map('n','<silent> gi', '<Plug>(coc-implementation)', {})
-map('n','<silent> gr', '<Plug>(coc-references)', {})
 
 map('n', '<leader>h', '<C-W><C-H>', {})
 map('n', '<leader>l', '<C-W><C-L>', {})
@@ -17,8 +13,6 @@ map('n', '<leader>k', '<C-W><C-K>', {})
 map('n', '<leader>t', ':botright terminal<Cr>', {})
 map('n', '<leader>+', ':vertical resize +5<CR>', {})
 map('n', '<leader>-', ':vertical resize -5<CR>', {})
-map('n', '<leader>ps', ':Rg<SPACE>', {})
-map('n', '<leader>f', ':FZF<CR>', {})
 
 -- Telescope keymaps
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
