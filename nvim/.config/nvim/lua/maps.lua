1fbf691139a601e1faf5de5c76e90709ccf63ce1
benjiemc <benjiemc@telus.net>
2022-10-26 12:05:05 +0100

    Add telescope as a plugin and configure key maps

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -1,4 +1,5 @@
 local map = vim.api.nvim_set_keymap
+local builtin = require('telescope.builtin')
 
 map('n', '<Space>', '', {})
 vim.g.mapleader = ' '
@@ -18,6 +19,12 @@ map('n', '<leader>+', ':vertical resize +5<CR>', {})
 map('n', '<leader>-', ':vertical resize -5<CR>', {})
 map('n', '<leader>ps', ':Rg<SPACE>', {})
 map('n', '<leader>f', ':FZF<CR>', {})
+
+-- Telescope keymaps
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 map('v', 'J', ":m '>+1<CR>gv=gv", {})
 map('v', 'K', ":m '<-2<CR>gv=gv", {})
