dc27801ec4a8c17d4bde2c56f5014cfaf728f116
benjiemc <benjiemc@telus.net>
2022-12-04 11:38:41 +0000

    Configure syntastic for flake8 linting on py files

--- nvim/.config/nvim/ftplugin/python.lua
+++ nvim/.config/nvim/ftplugin/python.lua
@@ -6,5 +6,6 @@ set.textwidth = 119
 set.colorcolumn = '120'
 vim.api.nvim_set_hl(0, 'ColorColumn', {ctermbg = 0, bg = 'lightgrey'})
 
-python_highlight_all = 1 
-syntastic_python_checkers = {'pylint', 'flake8'}
+vim.g.python_highlight_all = 1
+vim.g.syntastic_python_checkers = {'pylint', 'flake8'}
+vim.g.syntastic_python_flake8_args = "--max-line-length=120"
