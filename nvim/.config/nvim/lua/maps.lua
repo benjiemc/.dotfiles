73f7ba00445fc7bb7ab8950d58d00b81555d910c
benjiemc <benjiemc@telus.net>
2023-01-04 22:13:39 -0800

    Tidy maps

--- nvim/.config/nvim/lua/maps.lua
+++ nvim/.config/nvim/lua/maps.lua
@@ -1,24 +1,22 @@
-local map = vim.api.nvim_set_keymap
-local builtin = require('telescope.builtin')
-
-map('n', '<Space>', '', {})
 vim.g.mapleader = ' '
 
-options = { noremap = true }
+-- General
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
 
-map('n', '<leader>h', '<C-W><C-H>', {})
-map('n', '<leader>l', '<C-W><C-L>', {})
-map('n', '<leader>j', '<C-W><C-J>', {})
-map('n', '<leader>k', '<C-W><C-K>', {})
-map('n', '<leader>t', ':botright terminal<Cr>', {})
-map('n', '<leader>+', ':vertical resize +5<CR>', {})
-map('n', '<leader>-', ':vertical resize -5<CR>', {})
+-- Movement
+vim.keymap.set('n', '<leader>h', '<C-W><C-H>')
+vim.keymap.set('n', '<leader>l', '<C-W><C-L>')
+vim.keymap.set('n', '<leader>j', '<C-W><C-J>')
+vim.keymap.set('n', '<leader>k', '<C-W><C-K>')
+vim.keymap.set('n', '<leader>+', ':vertical resize +5<CR>')
+vim.keymap.set('n', '<leader>-', ':vertical resize -5<CR>')
 
 -- Telescope keymaps
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require('telescope.builtin')
 
-map('v', 'J', ":m '>+1<CR>gv=gv", {})
-map('v', 'K', ":m '<-2<CR>gv=gv", {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
+
