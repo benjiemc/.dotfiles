a94c3c42a1f85937995e92425faa0336822d0ce2
Benjamin McMaster <benjamin.mcmaster@rdm.ox.ac.uk>
2025-12-16 10:03:06 +0000

    Update neovim LSP configuration for 0.11

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,3 +1,3 @@
-require'lspconfig'.jedi_language_server.setup{}
-require'lspconfig'.clangd.setup{}
-require'lspconfig'.texlab.setup{}
+vim.lsp.config('jedi_language_server', {})
+vim.lsp.config('clangd', {})
+vim.lsp.config('texlab', {})
